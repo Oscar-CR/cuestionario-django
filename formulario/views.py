ee6d7ba2d41f25b1ad19b601afa0aa462a5f8ae5
Oscar-CR <krz.oficial@gmail.com>
2024-05-05 20:38:21 -0600

    agregado de ejemplo para enviar datos del controlador a la vista

--- formulario/views.py
+++ formulario/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hola")
+    titulo = 'Titulo de prueba'
+    return render(request, 'index.html', {
+        'titulo' : titulo
+    })
